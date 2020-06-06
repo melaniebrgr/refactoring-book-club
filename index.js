bf3902489ef7fb7d84840ff6f186c696e0c90156
Melanie Burger <melanie.burger@rangle.io>
2020-06-06 11:05:23 +0200

    🔧Configs package   🔧Adds run command

---
+++ index.js
@@ -0,0 +1,6 @@
+const plays = require("./chapter1/plays.json");
+const invoice = require("./chapter1/invoice.json");
+const statement = require("./chapter1/statement");
+
+const result = statement(invoice, plays);
+console.log(result);
