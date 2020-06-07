7eb42392b1815c5e0e037b3da4c7bcb52ce48093
Melanie Burger <melanie.burger@rangle.io>
2020-06-07 12:30:53 +0200

    🔖 p14

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -45,10 +45,10 @@ function statement(invoice, plays) {
       volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${
+    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
