e4ca7e1341ed82c5cf279fc07a10d0eaf6ce90f8
Melanie Burger <melanie.burger@rangle.io>
2020-06-07 16:47:44 +0200

    🔖 p33

--- chapter1/createStatementData.js
+++ chapter1/createStatementData.js
@@ -52,19 +52,11 @@ function createStatementData(invoice, plays) {
   }
 
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
 
