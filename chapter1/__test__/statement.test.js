b6bff0dfbe74625af45673d6da83c3158d28c53b
Melanie Burger <melanie.burger@rangle.io>
2020-06-07 16:40:45 +0200

    🔖 p32

--- chapter1/__test__/statement.test.js
+++ chapter1/__test__/statement.test.js
@@ -1,7 +1,11 @@
 const plays = require("../plays.json");
 const invoice = require("../invoice.json");
-const statement = require("../statement");
+const { statement, htmlStatement } = require("../statement");
 
 test("generates a statement", () => {
   expect(statement(invoice, plays)).toMatchSnapshot();
+});
+
+test("generates a HTML statement", () => {
+  expect(htmlStatement(invoice, plays)).toMatchSnapshot();
 });
