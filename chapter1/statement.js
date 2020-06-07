cc1eba441a0f90d7ea2dda90cf3de2129cdc2bd4
Melanie Burger <melanie.burger@rangle.io>
2020-06-07 15:30:02 +0200

    🔖 p26

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -1,13 +1,14 @@
 function statement(invoice, plays) {
   const statementData = {
     customer: invoice.customer,
+    performances: invoice.performances,
   };
-  return renderPlainText(statementData, invoice, plays);
+  return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
   let result = `Statement for ${data.customer}\n`;
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
@@ -38,7 +39,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
@@ -73,7 +74,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;
